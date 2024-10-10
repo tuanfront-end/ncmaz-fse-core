6e7b69feb874c682599da6fc65927b217520c52a
Chis Nghiax <atuanvptd@gmail.com>
2024-10-10 08:53:45 +0700

    update st

--- ncmaz-fse-core.php
+++ ncmaz-fse-core.php
@@ -18,7 +18,6 @@ if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
-
 define('NCMAZ_FSE_CORE_VERSION', '0.1.0');
 define('NCMAZ_FSE_CORE_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('NCMAZ_FSE_CORE_PLUGIN_BASE', plugin_basename(__FILE__));
@@ -27,9 +26,20 @@ define('NCMAZ_FSE_CORE_PLUGIN_ASSETS', NCMAZ_FSE_CORE_PLUGIN_URL . 'assets/');
 define('NCMAZ_FSE_CORE_PLUGIN_BUILD', NCMAZ_FSE_CORE_PLUGIN_URL . 'build/');
 define('NCMAZ_FSE_CORE_PLUGIN_SRC', NCMAZ_FSE_CORE_PLUGIN_URL . 'src/');
 define('NCMAZ_FSE_CORE_PLUGIN_INCLUDES', NCMAZ_FSE_CORE_PLUGIN_URL . 'includes/');
-define('NCMAZ_FSE_CORE_TEXT_DOMAIN',  'ncmfse');
-define('NCMAZ_FSE_CORE_STORE',  'ncmfse');
 
+// register_activation_hook function to run code when the plugin is activated.
+function ncmaz_fse_core_plugin_activation_function()
+{
+	if (get_option('ncmaz_fse_core_activation_created')) {
+		return;
+	}
+	// ...
+	update_option('ncmaz_fse_core_activation_created', true);
+}
+// Đăng ký hook kích hoạt
+register_activation_hook(__FILE__, 'ncmaz_fse_core_plugin_activation_function');
+
+// 
 function ncmaz_fse_core_register_blocks_init()
 {
 	$ncmfse_blocks = [
@@ -68,7 +78,6 @@ function ncmaz_fse_core_register_blocks_init()
 	}
 }
 add_action('init', 'ncmaz_fse_core_register_blocks_init');
-
 
 //  Import file utils.php
 require_once plugin_dir_path(__FILE__) . 'includes/hooks.php';
@@ -173,7 +182,6 @@ function ncmazfse_enable_linked_groups_render_block($block_content, $block, $ins
 	return $block_content;
 }
 add_filter('render_block_core/group', 'ncmazfse_enable_linked_groups_render_block', 10, 3);
-
 
 /**
  * Enable border for paragraph.
