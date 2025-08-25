59d273907a2ef8a57d401d4f4c36662764cd93f5
Chis Nghiax <atuanvptd@gmail.com>
2025-08-25 16:59:10 +0700

    update

--- build/post-media-play-button-block/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'f102af3dc000fc5f6d82', 'type' => 'module');
--- build/post-media-player-block/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'ceaf69ad1072e69c1d21', 'type' => 'module');
